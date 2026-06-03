cd705bc5c00079c79ecc0cf048224414d5b36a1f
Silly_coder <maityp394@gmail.com>
2026-06-04 03:26:36 +0530

    fix tsconfig

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -289,7 +289,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.13.0",
